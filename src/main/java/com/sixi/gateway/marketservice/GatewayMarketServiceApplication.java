e073467824bfac28c62ad54cf7bb9f381364c795
MiaoWoo <admin@yumc.pw>
2019-08-08 10:51:12 +0800

    Create & Init gateway-market-service Project...

---
+++ src/main/java/com/sixi/gateway/marketservice/GatewayMarketServiceApplication.java
@@ -0,0 +1,17 @@
+package com.sixi.gateway.marketservice;
+
+import org.springframework.boot.SpringApplication;
+
+import com.sixi.micro.common.annotation.SixiMicroServiceApplication;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @author MiaoWoo
+ */
+@SixiMicroServiceApplication
+public class GatewayMarketServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMarketServiceApplication.class, args);
+    }
+}
