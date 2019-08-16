1c6d467d430616798fdfab9c5d64e4bd9ed6eed9
zhangchongfei <icbc_838081@163.com>
2019-08-16 12:24:51 +0800

    fix：添加数据修改接口

--- src/main/java/com/sixi/gateway/marketservice/config/GatewayRouteValue.java
+++ src/main/java/com/sixi/gateway/marketservice/config/GatewayRouteValue.java
@@ -1,9 +1,11 @@
 package com.sixi.gateway.marketservice.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created with IDEA
@@ -13,19 +15,22 @@ import org.springframework.stereotype.Component;
  * @Date:2019/8/16
  * @Time:12:10
  */
+@Slf4j
 @Component
 public class GatewayRouteValue {
-    private static final String SERVICE = "/gateway.do";
+    private static final String key = "gateway:/gateway.do";
     @Autowired
     private GatewayConfig gatewayConfig;
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
 
-    @Bean
+    @PostConstruct
     private void saveGateWay() {
+        log.info("开始加载GatewayConfig到redis=========");
         String url = gatewayConfig.url;
-        redisTemplate.opsForValue().set(SERVICE, url);
+        redisTemplate.opsForValue().set(key, url);
+        log.info("加载GatewayConfig到redis完成=========");
     }
 
 }
