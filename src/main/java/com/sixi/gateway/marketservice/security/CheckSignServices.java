23b228759bd2eeac6d3bae4c8bdb9c860f8460cc
zhangyu <13100650510@163.com>
2019-08-23 09:31:32 +0800

    fix:过滤条件设置成功,实现转发VERSION2.0

--- src/main/java/com/sixi/gateway/marketservice/security/CheckSignServices.java
+++ src/main/java/com/sixi/gateway/marketservice/security/CheckSignServices.java
@@ -10,6 +10,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import reactor.core.publisher.Mono;
 
 import static com.sixi.gateway.checksigncommon.oauth.method.impl.SimpleAuthNonces.DEFAULT_MAX_TIMESTAMP_AGE;
 
@@ -41,8 +42,10 @@ public class CheckSignServices {
         } catch (AuthProblemException e) {
             //授权失败，不对其进行路由
             logger.error("Failed to CheckSign", e);
+            return (ServerHttpRequest) e;
         } catch (AuthException e) {
             logger.error("Failed to CheckSign", e);
+            return (ServerHttpRequest) e;
         }
         return req;
     }
